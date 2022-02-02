2b73ae386cc22cf1a44c5f911d2d9012f9a4e5a8
ottowan <ottowan@gmail.com>
2022-02-02 14:57:37 +0700

    [Create] make new proxy on go fiber

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	app.Use(logger.New())
 	app.Use(cors.New())
 
-	app.Get("/", proxy.Forward("http://localhost:10001"))
+	app.Get("/api/v1/news", proxy.Forward("http://localhost:10001/api/v1/news"))
 
 	// app.Listen(":8080")
 
